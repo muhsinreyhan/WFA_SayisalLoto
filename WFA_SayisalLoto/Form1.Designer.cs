327e7d81876514f2804e99ae11dc173178acfd3f
muhsin reyhan <muhsinreyhan@hotmail.com>
2019-05-12 13:52:37 +0300

    Add project files.

---
+++ WFA_SayisalLoto/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WFA_SayisalLoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.listBox8 = new System.Windows.Forms.ListBox();
+            this.listBox9 = new System.Windows.Forms.ListBox();
+            this.listBox10 = new System.Windows.Forms.ListBox();
+            this.listBox11 = new System.Windows.Forms.ListBox();
+            this.listBox12 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(13, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 148);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(139, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 148);
+            this.listBox2.TabIndex = 0;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 18;
+            this.listBox3.Location = new System.Drawing.Point(265, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 148);
+            this.listBox3.TabIndex = 0;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 18;
+            this.listBox4.Location = new System.Drawing.Point(391, 12);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 148);
+            this.listBox4.TabIndex = 0;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 18;
+            this.listBox5.Location = new System.Drawing.Point(517, 12);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(120, 148);
+            this.listBox5.TabIndex = 0;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 18;
+            this.listBox6.Location = new System.Drawing.Point(643, 12);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(120, 148);
+            this.listBox6.TabIndex = 0;
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.ItemHeight = 18;
+            this.listBox7.Location = new System.Drawing.Point(13, 166);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(120, 148);
+            this.listBox7.TabIndex = 0;
+            // 
+            // listBox8
+            // 
+            this.listBox8.FormattingEnabled = true;
+            this.listBox8.ItemHeight = 18;
+            this.listBox8.Location = new System.Drawing.Point(139, 166);
+            this.listBox8.Name = "listBox8";
+            this.listBox8.Size = new System.Drawing.Size(120, 148);
+            this.listBox8.TabIndex = 0;
+            // 
+            // listBox9
+            // 
+            this.listBox9.FormattingEnabled = true;
+            this.listBox9.ItemHeight = 18;
+            this.listBox9.Location = new System.Drawing.Point(265, 166);
+            this.listBox9.Name = "listBox9";
+            this.listBox9.Size = new System.Drawing.Size(120, 148);
+            this.listBox9.TabIndex = 0;
+            // 
+            // listBox10
+            // 
+            this.listBox10.FormattingEnabled = true;
+            this.listBox10.ItemHeight = 18;
+            this.listBox10.Location = new System.Drawing.Point(391, 166);
+            this.listBox10.Name = "listBox10";
+            this.listBox10.Size = new System.Drawing.Size(120, 148);
+            this.listBox10.TabIndex = 0;
+            // 
+            // listBox11
+            // 
+            this.listBox11.FormattingEnabled = true;
+            this.listBox11.ItemHeight = 18;
+            this.listBox11.Location = new System.Drawing.Point(517, 166);
+            this.listBox11.Name = "listBox11";
+            this.listBox11.Size = new System.Drawing.Size(120, 148);
+            this.listBox11.TabIndex = 0;
+            // 
+            // listBox12
+            // 
+            this.listBox12.FormattingEnabled = true;
+            this.listBox12.ItemHeight = 18;
+            this.listBox12.Location = new System.Drawing.Point(643, 166);
+            this.listBox12.Name = "listBox12";
+            this.listBox12.Size = new System.Drawing.Size(120, 148);
+            this.listBox12.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(750, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ÜRET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 389);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox12);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox11);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox10);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox9);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox8);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.ListBox listBox8;
+        private System.Windows.Forms.ListBox listBox9;
+        private System.Windows.Forms.ListBox listBox10;
+        private System.Windows.Forms.ListBox listBox11;
+        private System.Windows.Forms.ListBox listBox12;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WFA_SayisalLoto/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_SayisalLoto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            foreach (Control kontrol in this.Controls)
+            {
+                if (kontrol is ListBox)
+                {
+                    ListBox lst = (ListBox)kontrol;
+                    lst.Items.Clear();
+                }
+            }
+
+            foreach (Control ctrl in this.Controls)
+            {
+                for (int i = 1; i < 7; i++)
+                {
+                    int sayi = rnd.Next(1, 50);
+
+                    if (ctrl is ListBox)
+                    {
+                        ListBox lst = (ListBox)ctrl;
+
+                        if (lst.Items.Contains(sayi))
+                        {
+                            i--;
+                            continue;
+                        }
+                        lst.Items.Add(sayi);
+                    }
+                }
+            }
+        }
+    }
+}
